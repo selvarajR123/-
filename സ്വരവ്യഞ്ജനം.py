807c3a7d9f5de0150a0515ae29898a6771cc587a
selvarajR123 <97497262+selvarajR123@users.noreply.github.com>
2022-03-19 15:16:05 +0530

    Add files via upload

---
+++ സ്വരവ്യഞ്ജനം.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+ch = input('ഒരു സ്വനം നല്കുക: ')
+
+
+if(ch=='അ' or ch=='അ' or ch=='ആ' or ch =='ഇ' 
+            or ch=='ഈ' or ch=='ഉ' or ch=='ഊ' 
+            or ch=='ഋ' or ch=='എ' or ch=='ഏ' 
+            or ch=='ഐ' or ch=='ഒ' or ch=='ഓ' 
+              or ch=='ഔ' or ch=='അം' or ch=='അ'):
+    print(ch, "ഇതൊരു സ്വരാക്ഷരമാണ്")
+else:
+    print(ch, "ഇതൊരു വ്യഞ്ജനാക്ഷരമാണ്")
+
+
+# In[ ]:
+
+
+
+
